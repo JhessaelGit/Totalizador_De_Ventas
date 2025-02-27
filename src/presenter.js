fb0a172a0d9aa8bdb4bbcf3a2a76e01b9b28c255
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-27 13:32:42 -0400

    Refactoring: Nombres mas descriptivos a la hora de optener mas funciones

--- src/presenter.js
+++ src/presenter.js
@@ -2,13 +2,13 @@ import precio_neto from "./Precio_Neto";
 const cantidad_items = document.querySelector("#cantidad-item");
 const precio_items = document.querySelector("#precio-item");
 const formTotalizador=document.querySelector("#totalizador-form")
-const mostrarcantidaddiv=document.querySelector("#mostrar-items-div")
-const mostrarpreciodiv=document.querySelector("#mostrar-precio-items-div")
+const mostrarCantidadDiv=document.querySelector("#mostrar-cantidad-items-div")
+const mostrarDrecioDiv=document.querySelector("#mostrar-precio-items-div")
 formTotalizador.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cantidad_items_value = Number.parseInt(cantidad_items.value);
   const precio_items_value = Number.parseInt(precio_items.value);
-  mostrarcantidaddiv.innerHTML = "<p>" + "Cantidad de Items: " + cantidad_items_value + "</p>";
-  mostrarpreciodiv.innerHTML = "<p>" + "Precio por items: " + precio_items_value + "</p>";
+  mostrarCantidadDiv.innerHTML = "<p>" + "Cantidad de Items: " + cantidad_items_value + "</p>";
+  mostrarDrecioDiv.innerHTML = "<p>" + "Precio por items: " + precio_items_value + "</p>";
 });
