677c3396b5d1b329122a9cfca5a0a77436cca783
Jhessael Siancas <jhessael.siancas@ucb.edu.bo>
2025-02-27 13:39:09 -0400

    feat: Ahora muestra el Precio Neto

--- src/presenter.js
+++ src/presenter.js
@@ -4,11 +4,14 @@ const precio_items = document.querySelector("#precio-item");
 const formTotalizador=document.querySelector("#totalizador-form")
 const mostrarCantidadDiv=document.querySelector("#mostrar-cantidad-items-div")
 const mostrarDrecioDiv=document.querySelector("#mostrar-precio-items-div")
+const mostrarPrecioNetoDiv=document.querySelector("#mostrar-precio-neto-div")
+
 formTotalizador.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const cantidad_items_value = Number.parseInt(cantidad_items.value);
   const precio_items_value = Number.parseInt(precio_items.value);
   mostrarCantidadDiv.innerHTML = "<p>" + "Cantidad de Items: " + cantidad_items_value + "</p>";
-  mostrarDrecioDiv.innerHTML = "<p>" + "Precio por items: " + precio_items_value + "</p>";
+  mostrarDrecioDiv.innerHTML = "<p>" + "Precio por items: $" + precio_items_value + "</p>";
+  mostrarPrecioNetoDiv.innerHTML = "<p>" + "Precio Neto: $" + precio_neto(cantidad_items_value,precio_items_value) + "</p>";
 });
