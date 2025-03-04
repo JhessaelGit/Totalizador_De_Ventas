bdc64fd35977c21733d6643e0b402a4fef184fa2
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-03-04 16:52:41 -0400

    test: Descuento para 1000 funcionando

--- src/Descuentos.js
+++ src/Descuentos.js
@@ -1,5 +1,8 @@
 function descuento_correspondiente(cantidad)
 {
+    if (cantidad === 1000){
+        return 3
+    }
     return 0
 }
 export {descuento_correspondiente}
--- src/Descuentos.test.js
+++ src/Descuentos.test.js
@@ -3,4 +3,8 @@ describe("Test para la funcion de optener el descuento", () => {
     it("Al ingresar un 500 deve devolver un 0", () => {
         expect(descuento_correspondiente(500)).toEqual(0)
     })
+    it("Al ingresar un 1000 deve devolver un 3", () => {
+        expect(descuento_correspondiente(1000)).toEqual(3)
+    })
+    
 })
