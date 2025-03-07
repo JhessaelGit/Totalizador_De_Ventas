dde5ec420d59299bd3c99122d71fd1d1a32bfa36
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-03-07 17:43:26 -0400

    feat: Ahora selecciona y muestra las catergorias

--- src/presenter.js
+++ src/presenter.js
@@ -5,13 +5,15 @@ const cantidad_items = document.querySelector("#cantidad-item");
 const precio_items = document.querySelector("#precio-item");
 const formTotalizador=document.querySelector("#totalizador-form")
 const codigo_estado = document.querySelector("#codigo-estado");
+const categoria_producto = document.querySelector("#categoria-producto")
+
 const mostrarCantidadDiv=document.querySelector("#mostrar-cantidad-items-div")
 const mostrarPrecioDiv=document.querySelector("#mostrar-precio-items-div")
 const mostrarPrecioNetoDiv=document.querySelector("#mostrar-precio-neto-div")
 const mostrarCodigoEstadoDiv=document.querySelector("#mostrar-codigo-estado-div")
 const mostrarPrecioTotalDiv=document.querySelector("#mostrar-precio-total-div")
-const mostrarDesuentoDiv = document.querySelector("#mostrar-descuento-div")
-
+const mostrarDesuentoDiv = document.querySelector("#mostrar-categoria-div")
+const mostrarCategoriaDiv = document.querySelector('#mostrar-categoria-div')
 
 
 formTotalizador.addEventListener("submit", (event) => {
@@ -24,12 +26,14 @@ formTotalizador.addEventListener("submit", (event) => {
   const precio_neto=calcular_precio_neto(cantidad_items_value,precio_items_value);
   const descuento_porcentaje = descuento_correspondiente(cantidad_items_value)
   const precio_total= aplicar_descuento(aplicar_impuesto(precio_neto,impuesto_correspondiente),descuento_porcentaje)
+  const categoria_producto_value = categoria_producto.value;
 
   mostrarCantidadDiv.innerHTML = "<p>" + "Cantidad de Items: " + cantidad_items_value + "</p>";
   mostrarPrecioDiv.innerHTML = "<p>" + "Precio por items: $" + precio_items_value + "</p>";
   mostrarPrecioNetoDiv.innerHTML = "<p>" + "Precio Neto: $" + precio_neto + "</p>";
   mostrarDesuentoDiv.innerHTML = "<p>" + `Descuento (${descuento_porcentaje})%): ` + Math.round(precio_neto*(descuento_porcentaje/100)) +"</p>"
   mostrarCodigoEstadoDiv.innerHTML = "<p>" + "Impuesto para " + codigo_estado_value + ` (${impuesto_correspondiente}%): ` + ((precio_neto-aplicar_impuesto(precio_neto,impuesto_correspondiente))*-1) + "</p>";
+  mostrarCategoriaDiv.innerHTML = "<p>" + `El impuesto adicional para la categoria ${categoria_producto_value} es: ` +"%" + "</p>"
   mostrarPrecioTotalDiv.innerHTML = "<p>" + "Precio Total (impuesto y descuento): " + precio_total + "</p>";
 
 });
