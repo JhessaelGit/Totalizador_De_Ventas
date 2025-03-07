0f45a56e07bbccd957cc2dd9f6d5c53d5acf0e12
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-03-07 18:04:20 -0400

    feat: se muestra el porcentaje de impuesto por categoria alimentos

--- src/presenter.js
+++ src/presenter.js
@@ -1,5 +1,5 @@
 import calcular_precio_neto from "./Precio_Neto";
-import {impuesto_correspondiente_al_estado,aplicar_impuesto} from "./Impuestos_aplicados";
+import {impuesto_correspondiente_al_estado,aplicar_impuesto,impuesto_correspondiente_a_la_categoria} from "./Impuestos_aplicados";
 import { descuento_correspondiente, aplicar_descuento } from "./Descuentos";
 const cantidad_items = document.querySelector("#cantidad-item");
 const precio_items = document.querySelector("#precio-item");
@@ -27,13 +27,15 @@ formTotalizador.addEventListener("submit", (event) => {
   const descuento_porcentaje = descuento_correspondiente(cantidad_items_value)
   const precio_total= aplicar_descuento(aplicar_impuesto(precio_neto,impuesto_correspondiente),descuento_porcentaje)
   const categoria_producto_value = categoria_producto.value;
+  const porcentaje_impuesto_categoria = impuesto_correspondiente_a_la_categoria(categoria_producto_value)
+
 
   mostrarCantidadDiv.innerHTML = "<p>" + "Cantidad de Items: " + cantidad_items_value + "</p>";
   mostrarPrecioDiv.innerHTML = "<p>" + "Precio por items: $" + precio_items_value + "</p>";
   mostrarPrecioNetoDiv.innerHTML = "<p>" + "Precio Neto: $" + precio_neto + "</p>";
   mostrarDesuentoDiv.innerHTML = "<p>" + `Descuento (${descuento_porcentaje})%): ` + Math.round(precio_neto*(descuento_porcentaje/100)) +"</p>"
   mostrarCodigoEstadoDiv.innerHTML = "<p>" + "Impuesto para " + codigo_estado_value + ` (${impuesto_correspondiente}%): ` + ((precio_neto-aplicar_impuesto(precio_neto,impuesto_correspondiente))*-1) + "</p>";
-  mostrarCategoriaDiv.innerHTML = "<p>" + `El impuesto adicional para la categoria ${categoria_producto_value} es: ` +"%" + "</p>"
+  mostrarCategoriaDiv.innerHTML = "<p>" + `El impuesto adicional para la categoria ${categoria_producto_value} (${porcentaje_impuesto_categoria}%): ` + "</p>"
   mostrarPrecioTotalDiv.innerHTML = "<p>" + "Precio Total (impuesto y descuento): " + precio_total + "</p>";
 
 });
