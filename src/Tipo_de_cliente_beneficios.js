300350a99f723c87cdc3782d03880cc5890d2c18
Tairin-Munoz <tairinmunoz14@gmail.com>
2025-03-09 17:26:56 -0400

    feat: muestra el descuento en precio de envio segun el  tipo de cliente antiguo recurrente

--- src/Tipo_de_cliente_beneficios.js
+++ src/Tipo_de_cliente_beneficios.js
@@ -4,6 +4,10 @@ function obtener_beneficio_segun_tipo_de_cliente (tipo_de_cliente)
     {
         return 0;
     }
-    return 0.5;
+    else if (tipo_de_cliente === "Recurrente")
+    {
+        return 0.5;
+    }
+    return 1;
 }
 export {obtener_beneficio_segun_tipo_de_cliente}
--- src/Tipo_de_cliente_beneficios.test.js
+++ src/Tipo_de_cliente_beneficios.test.js
@@ -8,5 +8,10 @@ describe("prueba de la funcion de obtener beneficio segun el tipo de cliente", (
     it ("si la funcion obtiene el valor Recurrente debe devolver el dato de 0,5", () => {
         expect(obtener_beneficio_segun_tipo_de_cliente("Recurrente")).toEqual(0.5)
     })
+
+    it ("si la funcion obtiene el valor Antiguo recurrente debe devolver el dato de 1", () => {
+        expect(obtener_beneficio_segun_tipo_de_cliente("Antiguo recurrente")).toEqual(1)
+    })
 })
+
 
