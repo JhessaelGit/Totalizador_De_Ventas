53a2a474bd75357df257819dca124aa0345f875e
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-03-09 15:48:54 -0400

    Feat: Muestra el costo de envio para el peso volumentrico entre 0 y 10

--- src/Costo_Extra.test.js
+++ src/Costo_Extra.test.js
@@ -3,4 +3,11 @@ describe("Pruebas para la Funcion para obtener el costo de envio", () => {
     it("Al mandarle 0 debe devolver un 0", () => {
         expect(obtener_costo_envio_por_peso_volumetrico(0)).toEqual(0);
     })
+    it("Al mandarle un numero entre 0 y 10 debe devolver un 0", () => {
+        expect(obtener_costo_envio_por_peso_volumetrico(0)).toEqual(0);
+        expect(obtener_costo_envio_por_peso_volumetrico(4)).toEqual(0);
+        expect(obtener_costo_envio_por_peso_volumetrico(7)).toEqual(0);
+        expect(obtener_costo_envio_por_peso_volumetrico(10)).toEqual(0);  
+    })
+    
 })
