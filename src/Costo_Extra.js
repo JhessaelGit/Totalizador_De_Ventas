4c3bd0940684869fcbed4a0e48938cd46bb4489c
Jhessael Siancas <siancas.jhessael@gmail.com>
2025-03-09 15:55:53 -0400

    Feat: Muestra el costo de envio para peso volumetrico entre 21 y 40

--- src/Costo_Extra.js
+++ src/Costo_Extra.js
@@ -1,7 +1,9 @@
 function obtener_costo_envio_por_peso_volumetrico(peso_volumetrico)
 {
-    if (peso_volumetrico > 10)
+    if (peso_volumetrico > 10 && peso_volumetrico <=20)
         return 3.5
+    else if( peso_volumetrico > 20)
+        return 5
     return 0
 }
 
--- src/Costo_Extra.test.js
+++ src/Costo_Extra.test.js
@@ -15,5 +15,11 @@ describe("Pruebas para la Funcion para obtener el costo de envio", () => {
         expect(obtener_costo_envio_por_peso_volumetrico(18)).toEqual(3.5);
         expect(obtener_costo_envio_por_peso_volumetrico(20)).toEqual(3.5);  
     })
+    it("Al mandarle un numero entre 21 y 40 debe devolver un 5", () => {
+        expect(obtener_costo_envio_por_peso_volumetrico(21)).toEqual(5);
+        expect(obtener_costo_envio_por_peso_volumetrico(28)).toEqual(5);
+        expect(obtener_costo_envio_por_peso_volumetrico(36)).toEqual(5);
+        expect(obtener_costo_envio_por_peso_volumetrico(40)).toEqual(5);  
+    })
     
 })
